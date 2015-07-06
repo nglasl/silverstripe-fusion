7178e569bd0c5e2a03af171e2f01b97c560444f3
Nathan Glasl <nathan@silverstripe.com.au>
2015-07-06 11:06:59 +1000

    :rainbow:

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This module is currently in progress.
 
 ## TO DO - PAGE SEARCH
 
-- Search against FusionTags, then loop through each subclass of SiteTree to determine other relationship fields to search on, based on parseTags.
+- Search against FusionTags.
--- code/extensions/SiteTreeTagsExtension.php
+++ code/extensions/SiteTreeTagsExtension.php
@@ -16,7 +16,7 @@ class SiteTreeTagsExtension extends DataExtension {
 		foreach(singleton('FusionTag')->parseTags() as $tag => $field) {
 			$output[$tag] = $tag;
 		}
-		$intersect = array_intersect($output, $this->owner->many_many());
+		$intersect = array_intersect($this->owner->many_many(), $output);
 
 		// if there are no "fused" tags found in the many_many relationship, add the fusion tagging
 
@@ -29,6 +29,42 @@ class SiteTreeTagsExtension extends DataExtension {
 				'Tags',
 				FusionTag::get()->map()->toArray()
 			)->setMultiple(true));
+		}
+	}
+
+	public function onBeforeWrite() {
+
+		parent::onBeforeWrite();
+
+		// search for fusion tags that we can use for searching, retrieving the relationship names
+
+		$output = array();
+		foreach(singleton('FusionTag')->parseTags() as $tag => $field) {
+			$output[$tag] = $tag;
+		}
+		$intersect = array_intersect($this->owner->many_many(), $output);
+
+		// if there are "fused" tags found in the many_many relationship, write these into FusionTags, so we have a single search field
+
+		if(!empty($intersect)) {
+
+			// clear the fusion tags out so we can keep it completely in sync
+
+			$this->owner->FusionTags()->removeAll();
+
+			// retrieve each relationship
+
+			foreach($intersect as $relationship => $tags) {
+
+				// retrieve each tag
+
+				foreach($this->owner->$relationship() as $tag) {
+
+					// retrieve the associated fusion tag and push this into FusionTags
+
+					$this->owner->FusionTags()->add($tag->Fusion());
+				}
+			}
 		}
 	}
 
