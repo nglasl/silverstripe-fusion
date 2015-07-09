4dd735fa44cab6d329e4c238c700cb3912717f2e
Nathan Glasl <nathan@silverstripe.com.au>
2015-07-09 16:26:53 +1000

    Implementing the ability to search content tagging in the CMS.

---
+++ code/extensions/CMSMainTaggingExtension.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ *	This extension will automatically be applied, allowing CMS searchable content tagging.
+ *	@author Nathan Glasl <nathan@silverstripe.com.au>
+ */
+
+class CMSMainTaggingExtension extends Extension {
+
+	/**
+	 *	Update the page filters, allowing CMS searchable content tagging.
+	 */
+
+	public function updateSearchForm($form) {
+
+		// Instantiate a field containing existing tags.
+
+		$form->Fields()->insertBefore(ListboxField::create(
+			'q[Tagging]',
+			'Tagging',
+			FusionTag::get()->map()->toArray()
+		)->setMultiple(true), 'q[Term]');
+
+		// Update the page filters, allowing tags to be parsed correctly.
+
+		$form->setFormAction(null);
+		$form->Actions()->replaceField('action_doSearch', FormAction::create(
+			'updateSearchFilter',
+			_t('CMSMain_left_ss.APPLY_FILTER', 'Apply Filter')
+		)->addExtraClass('ss-ui-action-constructive'));
+	}
+
+	/**
+	 *	Update the page filters, allowing tags to be parsed correctly.
+	 *
+	 *	@parameter <{SEARCH_FORM_DATA}> array
+	 */
+
+	public function updateSearchFilter($data) {
+
+		$link = $this->owner->Link();
+		$separator = '?';
+
+		// Determine whether page filters exist.
+
+		if(isset($data['q'])) {
+			foreach($data['q'] as $filter => $value) {
+
+				// Determine whether tagging filters exist.
+
+				if($filter === 'Tagging' && is_array($value)) {
+
+					// Parse the tagging into a searchable format.
+
+					$tagging = array();
+					foreach($value as $tag) {
+
+						// Retrieve the tag title.
+
+						$tagging[] = FusionTag::get()->byID($tag)->Title;
+					}
+					$value = implode(' ', $tagging);
+				}
+
+				// Append the filters to the URL.
+
+				$link = HTTP::setGetVar("q[{$filter}]", $value, $link, $separator);
+				$separator = '&';
+			}
+		}
+		return $this->owner->redirect(urlencode($link));
+	}
+
+}
